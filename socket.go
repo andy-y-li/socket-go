5ab3485c49477e131f72769452867f5776a81efb
Andy <andy.li@b-padynamics.com>
2018-05-22 12:22:20 +0800

    update read/write.

--- socket.go
+++ socket.go
@@ -18,25 +18,30 @@ func PrintLog(format string, args ...interface{}) {
 }
 
 func Read(conn net.Conn) (string, error) {
-	readBytes := make([]byte, 1)
-	var buffer bytes.Buffer
-	for {
-		_, err := conn.Read(readBytes)
-		if err != nil {
-			return "", err
-		}
-		readByte := readBytes[0]
-		if readByte == DELIMITER {
-			break
-		}
-		buffer.WriteByte(readByte)
+	readBytes := make([]byte, 65535)
+	//var buffer bytes.Buffer
+	//for {
+	n, err := conn.Read(readBytes)
+	if err != nil {
+		return "", err
 	}
-	return buffer.String(), nil
+	return string(readBytes[:n]), nil
+	/*
+			readByte := readBytes[0]
+			if readByte == DELIMITER ||
+				readByte == '\r' ||
+				readByte == '\n' {
+				break
+			}
+			buffer.WriteByte(readByte)
+		//}
+	*/
+	//return buffer.String(), nil
 }
 
 func Write(conn net.Conn, content string) (int, error) {
 	var buffer bytes.Buffer
 	buffer.WriteString(content)
-	buffer.WriteByte(DELIMITER)
+	//buffer.WriteByte(DELIMITER)
 	return conn.Write(buffer.Bytes())
 }
